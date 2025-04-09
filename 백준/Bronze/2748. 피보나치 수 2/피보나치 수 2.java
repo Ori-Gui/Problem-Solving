b499fd775032393af41a957ff6151387f3a7504e
양준하 <100835168+Ori-Gui@users.noreply.github.com>
2025-04-09 11:39:36 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 100 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+	static int N;
+	static long[] dp;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		dp = new long[N+1];
+		dp[0] = 0;
+		dp[1] = 1;
+
+		for (int n = 2; n <= N; n++) {
+			dp[n] = dp[n-1] + dp[n-2];
+		}
+
+		System.out.println(dp[N]);
+	}
+}
