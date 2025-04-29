a2b4f5d6b46b6db360da9a8ffe1e5237b46913e5
Ori-Gui <100835168+Ori-Gui@users.noreply.github.com>
2025-04-29 17:13:16 +0900

    [level 3] Title: N으로 표현, Time: 2865.02 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -0,0 +1,91 @@
+import java.util.*;
+
+class Solution {
+    
+    public int solution(int N, int number) {
+        int[] dp = new int[32001];
+        Arrays.fill(dp, 32000);
+        
+        if (N != 1) {
+            dp[N] = 1;
+            dp[N - N] = 2; // 0
+            dp[N / N] = 2; // 1
+            dp[N + N] = 2;
+            dp[N * N] = 2;
+            
+            int temp = N * 10 + N;
+            int k = 2;
+            while (true) {
+                if (temp > 32000) {
+                    break;
+                }
+                dp[temp] = k; 
+                temp = temp * 10 + N;
+                k += 1;
+            }
+            for (int a = 0; a < 3; a++) {
+                for (int i = 0; i < 10000; i++) {
+                    for (int j = 0; j < 10000; j++) {
+                        if (dp[i] == 32000 && dp[j] == 32000) continue;
+
+                        if (i+j <= 32000) {
+                            dp[i+j] = Math.min(dp[i+j], dp[i] + dp[j]);
+                        }           
+                        if (i-j >= 0) {
+                            dp[i-j] = Math.min(dp[i-j], dp[i] + dp[j]);
+                        }
+
+                        if (i*j <= 32000) {
+                            dp[i*j] = Math.min(dp[i*j], dp[i] + dp[j]);
+                        }
+
+                        if (i != 0 && j != 0) {
+                            dp[i/j] = Math.min(dp[i/j], dp[i] + dp[j]);
+                        }
+                    }
+                }
+            }
+        } else {
+            dp[N] = 1;
+            dp[N - N] = 2; // 0
+            dp[N + N] = 2;
+            
+            int temp = N * 10 + N;
+            int k = 2;
+            while (true) {
+                if (temp > 32000) {
+                    break;
+                }
+                dp[temp] = k; 
+                temp = temp * 10 + N;
+                k += 1;
+            }
+            
+            for (int a = 0; a < 3; a++) {
+                for (int i = 0; i < 10000; i++) {
+                    for (int j = 0; j < 10000; j++) {
+                        if (dp[i] == 32000 && dp[j] == 32000) continue;
+
+                        if (i+j <= 32000) {
+                            dp[i+j] = Math.min(dp[i+j], dp[i] + dp[j]);
+                        }           
+                        if (i-j >= 0) {
+                            dp[i-j] = Math.min(dp[i-j], dp[i] + dp[j]);
+                        }
+
+                        if (i*j <= 32000) {
+                            dp[i*j] = Math.min(dp[i*j], dp[i] + dp[j]);
+                        }
+
+                        if (i != 0 && j != 0) {
+                            dp[i/j] = Math.min(dp[i/j], dp[i] + dp[j]);
+                        }
+                    }
+                }
+            }
+        }
+             
+        int answer = dp[number] > 8 ? -1 : dp[number];
+        return answer;
+    }
+}
