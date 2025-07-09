739716f7d5cbcc1f8524d206e8127648db507dc5
Ori-Gui <100835168+Ori-Gui@users.noreply.github.com>
2025-07-09 12:42:31 +0900

    [Silver III] Title: 주유소, Time: 316 ms, Memory: 30704 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int[] dist;
+    static int[] cities;
+    static int ans;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        dist = new int[N-1];
+        cities = new int[N];
+        ans = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N-1; i++) {
+            dist[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            cities[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int curi = 0;
+        while (curi < N-1) {
+            int d = 1;
+
+            for (int i = curi + 1; i < N-1; i++) {
+                if (cities[curi] < cities[i]) {
+                    d += 1;
+                } else {
+                    break;
+                }
+            }
+
+            for (int i = curi; i < curi + d; i++) {
+                ans += cities[curi] * dist[i];
+            }
+            curi += d;
+        }
+        System.out.println(ans);
+    }
+}
