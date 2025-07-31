991504cd5fe7084cd92b9f5887aca8a83f3a5295
Ori-Gui <100835168+Ori-Gui@users.noreply.github.com>
2025-07-31 20:05:01 +0900

    [Silver II] Title: 창고 다각형, Time: 208 ms, Memory: 18360 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -12,11 +12,6 @@ public class Main {
             this.l = l;
             this.h = h;
         }
-
-        @Override
-        public String toString() {
-            return "[" + l + ", " + h + "]";
-        }
     }
 
     public static void main(String[] args) throws IOException {
