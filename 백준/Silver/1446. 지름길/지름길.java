a08380f80fb801a28bf9789c989aed355d0931ca
Ori-Gui <100835168+Ori-Gui@users.noreply.github.com>
2025-08-08 23:25:13 +0900

    [Silver I] Title: 지름길, Time: 188 ms, Memory: 18460 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.java
+++ 백준/Silver/1446. 지름길/지름길.java
@@ -1,47 +1,50 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int N, D;
+    static List<ShortCut> shortcuts = new ArrayList<>();
+    static int[] dp;
 
-    public static class ShortCut {
+    static class ShortCut {
         int s;
         int e;
         int d;
 
-        public ShortCut(int s, int e, int d) {
+        ShortCut(int s, int e, int d) {
             this.s = s;
             this.e = e;
             this.d = d;
         }
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = sc.nextInt();
-        int D = sc.nextInt();
-        List<ShortCut> shortcuts = new ArrayList<>();
-        int dp[] = new int[D + 1];
+        N = Integer.parseInt(st.nextToken());
+        D = Integer.parseInt(st.nextToken());
+        dp = new int[D + 1];
 
         for (int i = 1; i <= D; i++) {
             dp[i] = i;
         }
 
         for (int i = 0; i < N; i++) {
-            int s = sc.nextInt();
-            int e = sc.nextInt();
-            int d = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
             shortcuts.add(new ShortCut(s, e, d));
         }
 
-        // 지름길 end 기준 -> 지름길 start 기준 (오름차순 정렬), dp계산 순서대로 하기
-        shortcuts.sort(Comparator.comparingInt((ShortCut shortCut) -> shortCut.e));
+        shortcuts.sort(Comparator.comparingInt((ShortCut c) -> c.e));
 
         for (int i = 0; i < N; i++) {
             ShortCut cur = shortcuts.get(i);
-
             if (cur.e <= D) {
                 dp[cur.e] = Math.min(dp[cur.e], dp[cur.s] + cur.d);
-                for (int j = cur.e; j < D + 1; j++) {
+                for (int j = cur.e + 1; j <= D; j++) {
                     dp[j] = Math.min(dp[j], dp[j - 1] + 1);
                 }
             }
