8f5b698d97841c6dd594c695ddf04a3f05f040e3
Ori-Gui <100835168+Ori-Gui@users.noreply.github.com>
2025-09-16 01:33:01 +0900

    [Gold IV] Title: 공유기 설치, Time: 256 ms, Memory: 29028 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -2,28 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int N, C;
+    static int ans = 0;
+    static int[] houses;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
+
+        N = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
         
-        int[] houses = new int[N];
+        houses = new int[N];
         for (int i = 0; i < N; i++) {
-            houses[i] = Integer.parseInt(br.readLine().trim());
+            houses[i] = Integer.parseInt(br.readLine());
         }
 
         Arrays.sort(houses);
         
-        // 이분 탐색
         int l = 1; 
-        int r = houses[N - 1] - houses[0]; // 최대 거리
-        int ans = 0;
+        int r = houses[N - 1] - houses[0];
         
         while (l <= r) {
             int mid = (l + r) / 2;
             
-            int cnt = 1; // 첫 번째 집에 무조건 공유기 설치
+            int cnt = 1;
             int wifi = houses[0];
             for (int i = 1; i < N; i++) {
                 if (houses[i] - wifi >= mid) {
@@ -33,11 +36,9 @@ public class Main {
             }
             
             if (cnt >= C) {
-                // 공유기 설치 가능 -> 거리 늘리기  
                 ans = mid;
                 l = mid + 1;
             } else {
-                // 공유기 설치가 불가 -> 거리 줄이기
                 r = mid - 1;
             }
         }
