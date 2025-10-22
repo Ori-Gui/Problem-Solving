cdb87fb1c4ed8bb8be54d0138b43bccb5572fa80
Ori-Gui <100835168+Ori-Gui@users.noreply.github.com>
2025-10-22 21:58:36 +0900

    [Silver I] Title: 주지수, Time: 1232 ms, Memory: 122280 KB -BaekjoonHub

---
+++ 백준/Silver/15724. 주지수/주지수.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M, K;
+    static int[][] map;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N+1][M+1];
+        dp = new int[N+1][M+1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        K = Integer.parseInt(br.readLine());
+
+        for (int x = 1; x <= N; x++) {
+                for (int y = 1; y <= M; y++) {
+                    dp[x][y] = map[x][y] + dp[x-1][y];
+                }
+            }
+
+        for (int x = 1; x <= N; x++) {
+            for (int y = 1; y <= M; y++) {
+                dp[x][y] += dp[x][y-1];
+            }
+        }
+        
+        for (int k = 0; k < K; k++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            System.out.println(dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1]);
+        }
+    }
+}
